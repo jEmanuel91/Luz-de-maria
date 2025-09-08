1327d786567c3d696e80d6b4d3057399a482afa9
jEmanuel91 <j.emanuel.050612@gmail.com>
2025-09-07 22:11:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const cards = document.querySelectorAll('.card');
 
 cards.forEach(card => {
   card.addEventListener('click', () => {
+    // Toggle do conteúdo do card
     cards.forEach(c => {
       if (c !== card) c.querySelector('.card-content').style.display = 'none';
     });
     const content = card.querySelector('.card-content');
     content.style.display = (content.style.display === 'block') ? 'none' : 'block';
+
+    // Adiciona/remover classe 'touch' para efeito de zoom em touch devices
+    card.classList.toggle('touch');
   });
 });
